08152dd966d5d2c2fdf54bead25d27cca07df7e0
Your Name <dan.weibren@gmail.com>
2025-10-30 11:10:11 +0200

    - connecting to atlas

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
     dbURL: process.env.MONGO_URL || 'mongodb+srv://danweibren_db_user:0g4S33V8lIRvyEo4@oneday.0rvqfsg.mongodb.net/?appName=OneDay',
-    dbName: process.env.DB_NAME || 'tester_db'
+    dbName: process.env.DB_NAME || 'oneDay_db'
 }
